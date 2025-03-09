1cc993c2e1f2eb8f4e399cea2fa4c2b6fcbf93cc
Juan Mireles <juanmireles@proton.me>
2025-03-08 19:47:11 -0500

    Initial commit

---
+++ pong-game/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ping_Pong
+{
+    class Ball : GameObject
+    {
+        protected float _DX;
+        public float DX
+        {
+            get { return _DX; }
+            set { _DX = value; }
+        }
+
+        protected float m_DY;
+        public float DY
+        {
+            get { return m_DY; }
+            set { m_DY = value; }
+        }
+    }
+}
